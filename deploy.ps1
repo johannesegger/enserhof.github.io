d095cecb31c3372184173cd2567d34d9bce858e3
Johannes Egger <j.egger@posteo.at>
2018-08-06 22:06:16 +0200

    Fix AppVeyor hanging build

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,13 @@
 $buildOutputDir = ".\build-tmp"
 
 git worktree add $buildOutputDir master
+Remove-Item $buildOutputDir -Exclude .git -Recurse -Force
 Copy-Item -Force .\public\** $buildOutputDir
 $commitHash = git rev-parse HEAD
 
 Push-Location $buildOutputDir
-git status
 git add .
+git status
 git commit -m "Build $commitHash"
 git push
 Pop-Location
