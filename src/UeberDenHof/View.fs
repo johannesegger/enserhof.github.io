f47fce5821c826931a83a5a311f9406145bef9b1
Johannes Egger <j.egger@posteo.at>
2018-08-07 08:28:04 +0200

    Add text to Über den Hof

--- src/UeberDenHof/View.fs
+++ src/UeberDenHof/View.fs
@@ -13,6 +13,20 @@ let root =
 
   [ Heading.h1 [ Heading.Is3 ]
       [ str "Über den Hof" ]
+    Content.content []
+      [ str "Der Enserhof ist seit vier Generationen ein Familienbetrieb."
+        br []
+        str "Früher wurde er vor allem zu Selbstversorgungszwecken genützt."
+        br []
+        str "Bis 1995 wurden hauptsächlich Kühe zur Milchproduktion gehalten."
+        br []
+        str "Zwischen 1995 und 2014 war der Betrieb dann eine reine Schweinezucht und -mast."
+        br []
+        str "Wir (Sylvia und Johannes) sind seit 2011 auf dem Hof."
+        br []
+        str "2012 sind die beiden Esel Lilly und Paula als Ausgleich vom Arbeitsalltag zu uns gekommen."
+        br []
+        str "2016 ist der Vierkanthof durch die Entfernung von zwei Seiten geöffnet worden und bietet seit dem Platz für unseren Garten." ]
     Heading.h2 [ Heading.Is4 ]
       [ str "Tiere" ]
     Tile.ancestor [ ]
