93ea342b12ad5cde254d38ebc8c398ec9a4488de
Johannes Egger <j.egger@posteo.at>
2018-08-08 22:32:13 +0200

    Don't wrap Aktivitäten text in Fulma content

--- src/Aktivitaeten/View.fs
+++ src/Aktivitaeten/View.fs
@@ -40,21 +40,20 @@ let root model =
     | LoadError (HttpError _e) ->
       [ str "Fehler beim Laden der nächsten Stallzeiten. Bitte auf der Tafel, die vor unserem Hoftor steht, ablesen." ]
 
-  [ Content.content []
-      [ Heading.h1 [ Heading.Is3 ]
-          [ str "Aktivitäten" ]
-        Heading.h2 [ Heading.Is4 ]
-          [ str "Stallarbeit erledigen" ]
-        Content.content []
-          [ p []
-              [ str "Ihr habt ab nun die Möglichkeit, mit uns in den Stall zu gehen."
-                br []
-                str "Wir reinigen gemeinsam die Koppel und die Ställe, pfücken Futter für die Hasen, heben gemeinsam frische Eier ab und füttern die Esel, Kühe und Ponys mit Heu."
-                br []
-                str "Das Stallgehen dauert ca. eine Stunde und findet bei jeder Witterung statt."
-                br []
-                str "Wir freuen uns, wenn ihr einfach mal vorbei schaut." ]
-            span [] stallzeitenContent
-            Image.image
-              [ Image.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Left) ] ]
-              [ img [ Src (importAll "../../images/stallarbeit.jpg"); Style [ MaxWidth "640px" ] ] ] ] ] ]
+  [ Heading.h1 [ Heading.Is3 ]
+      [ str "Aktivitäten" ]
+    Heading.h2 [ Heading.Is4 ]
+      [ str "Stallarbeit erledigen" ]
+    Content.content []
+      [ p []
+          [ str "Ihr habt ab nun die Möglichkeit, mit uns in den Stall zu gehen."
+            br []
+            str "Wir reinigen gemeinsam die Koppel und die Ställe, pfücken Futter für die Hasen, heben gemeinsam frische Eier ab und füttern die Esel, Kühe und Ponys mit Heu."
+            br []
+            str "Das Stallgehen dauert ca. eine Stunde und findet bei jeder Witterung statt."
+            br []
+            str "Wir freuen uns, wenn ihr einfach mal vorbei schaut." ]
+        span [] stallzeitenContent
+        Image.image
+          [ Image.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Left) ] ]
+          [ img [ Src (importAll "../../images/stallarbeit.jpg"); Style [ MaxWidth "640px" ] ] ] ] ]
