44354b78ffd0c4fd32442f4510f3d7ee59fd52ea
Johannes Egger <j.egger@posteo.at>
2018-08-12 22:12:48 +0200

    Build 3c4197d7d4b1428c9e348971c0ae95837e92fe0a

---
+++ precache-manifest.77f54a68dc630b053171023ceaa32c39.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "revision": "fae94e2d82734f8d8b2e",
+    "url": "client.js"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.77f54a68dc630b053171023ceaa32c39.js"
+);
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, workbox.strategies.cacheFirst({ cacheName: "images", plugins: [new workbox.expiration.Plugin({"maxAgeSeconds":604800,"purgeOnQuotaError":false})] }), 'GET');
+workbox.routing.registerRoute(/\/api\//, workbox.strategies.networkFirst({ cacheName: "api-cache", plugins: [] }), 'GET');
